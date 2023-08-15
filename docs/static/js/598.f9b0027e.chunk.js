9bd2a4c16c5996c06b1f3a65f456ee309d3b5dc0
pablomos <mosconpablo@gmail.com>
2023-08-15 19:42:00 -0300

    commit

--- docs/static/js/598.f9b0027e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproyecto_final_app=self.webpackChunkproyecto_final_app||[]).push([[598],{8598:function(p,e,a){a.r(e),e.default={}}}]);
-//# sourceMappingURL=598.f9b0027e.chunk.js.map
--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import {BrowserRouter} from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,22 +15,5 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-const StarRating = ({ rating }) => {
-  const totalStars = 5;
-  return (
-    <div>
-      {Array.from({ length: totalStars }, (_, index) => (
-        <FontAwesomeIcon
-          icon={faStar}
-          key={index}
-          color={index < rating ? 'gold' : 'gray'}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default StarRating;
 
 reportWebVitals();
